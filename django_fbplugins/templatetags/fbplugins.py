4711612a41a183c09ea98a991f5d6b6548c72dd8
Marco Pompili <marcs.pompili@gmail.com>
2014-03-17 00:01:35 +0100

    Added django dependency in setup.py

--- django_fbplugins/templatetags/fbplugins.py
+++ django_fbplugins/templatetags/fbplugins.py
@@ -1,21 +1,19 @@
-'''
+"""
 Created on 16/giu/2013
 
 @author: Marco Pompili
-'''
-
+"""
 
 from django import template
 from django.utils.translation import get_language
 
 register = template.Library()
 
-
-langs = { 
-         "it" : "it_IT",
-         "en" : "en_US",
-         "de" : "de_DE" 
-         }
+langs = {
+    "it": "it_IT",
+    "en": "en_US",
+    "de": "de_DE"
+}
 
 
 def fb_like_btn(href, **kwargs):
@@ -33,20 +31,21 @@ def fb_like_btn(href, **kwargs):
             {% fb_like_btn_html "https://www.facebook.com/FacebookDevelopers" %}
     
     """
-    
-    return { 
-        'lang' : langs[get_language()],
-        'href' : href,
-        'layout' : kwargs.get('layout','standard'),
-        'colorscheme' : kwargs.get('colorscheme', 'light'),
-        'action' : kwargs.get('action', 'like'),
-        'showfaces' : kwargs.get('showfaces', 'true'),
-        'share' : kwargs.get('share', 'true'),
-        'height' : kwargs.get('height', '80'),
-        'scrolling' : kwargs.get('scrolling', 'no'),
-        'frameborder' : kwargs.get('frameborder', '0'),
-        'allowTransparency' : kwargs.get('allowTransparency', 'true')
+
+    return {
+        'lang': langs[get_language()],
+        'href': href,
+        'layout': kwargs.get('layout', 'standard'),
+        'colorscheme': kwargs.get('colorscheme', 'light'),
+        'action': kwargs.get('action', 'like'),
+        'showfaces': kwargs.get('showfaces', 'true'),
+        'share': kwargs.get('share', 'true'),
+        'height': kwargs.get('height', '80'),
+        'scrolling': kwargs.get('scrolling', 'no'),
+        'frameborder': kwargs.get('frameborder', '0'),
+        'allowTransparency': kwargs.get('allowTransparency', 'true')
     }
+
 
 register.inclusion_tag('django_fbplugins/btn/iframe.html', name='fb_like_btn_iframe')(fb_like_btn)
 register.inclusion_tag('django_fbplugins/btn/html5.html', name='fb_like_btn_html')(fb_like_btn)
@@ -67,20 +66,21 @@ def fb_like_box(href, **kwargs):
             {% fb_like_box_html "https://www.facebook.com/FacebookDevelopers" %}
             
     """
-    
+
     return {
-        'lang' : langs[get_language()],
-        'href' : href,
-        'height' : kwargs.get('height', '290'),
-        'colorscheme' : kwargs.get('colorscheme', 'light'),
-        'showfaces' : kwargs.get('showfaces', 'true'),
-        'header' : kwargs.get('header', 'true'),
-        'stream' : kwargs.get('stream', 'false'),
-        'showborder' : kwargs.get('showborder', 'true'),
-        'scrolling' : kwargs.get('scrolling', 'no'),
-        'frameborder' : kwargs.get('frameborder', '0'),
-        'allowTransparency' : kwargs.get('allowTransparency', 'true')
+        'lang': langs[get_language()],
+        'href': href,
+        'height': kwargs.get('height', '290'),
+        'colorscheme': kwargs.get('colorscheme', 'light'),
+        'showfaces': kwargs.get('showfaces', 'true'),
+        'header': kwargs.get('header', 'true'),
+        'stream': kwargs.get('stream', 'false'),
+        'showborder': kwargs.get('showborder', 'true'),
+        'scrolling': kwargs.get('scrolling', 'no'),
+        'frameborder': kwargs.get('frameborder', '0'),
+        'allowTransparency': kwargs.get('allowTransparency', 'true')
     }
+
 
 register.inclusion_tag('django_fbplugins/box/iframe.html', name='fb_like_box_iframe')(fb_like_box)
 register.inclusion_tag('django_fbplugins/box/html5.html', name='fb_like_box_html')(fb_like_box)
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 24/gen/2014
 
 @author: Marco Pompili
-'''
+"""
 
 import os
 from setuptools import setup
@@ -12,26 +12,27 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-      name='django-facebook-social-plugins',
-      version='0.1',
-      packages=['django_fbplugins'],
-      include_package_data=True,
-      license='MIT License',
-      description='Django wrapper tags on facebook social plugins.',
-      long_description=README,
-      url='https://github.com/marcopompili/django-facebook-social-plugins',
-      author='Marco Pompili',
-      author_email='django@emarcs.org',
-      classifiers=[
-            'Environment :: Web Environment',
-            'Framework :: Django',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        ],
-      )
+    name='django-facebook-social-plugins',
+    version='0.1',
+    packages=['django_fbplugins'],
+    include_package_data=True,
+    license='MIT License',
+    description='Django wrapper tags on facebook social plugins.',
+    long_description=README,
+    url='https://github.com/marcopompili/django-facebook-social-plugins',
+    author='Marco Pompili',
+    author_email='django@emarcs.org',
+    install_requires = ['django'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
